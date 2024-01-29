2e74aedcb16893b6b06d1f8637b0c5c6849d50ff
NoahV17 <nvario1@gmail.com>
2024-01-29 14:49:36 -0500

    moved libraries to correct location and got edge detection working.

--- test_programs/main.py
+++ test_programs/main.py
@@ -1,7 +1,7 @@
 import cv2
  
 # Read the original image
-img = cv2.imread('test.jpg') 
+img = cv2.imread('test_programs/mountain.png') 
 # Display original image
 cv2.imshow('Original', img)
 cv2.waitKey(0)
